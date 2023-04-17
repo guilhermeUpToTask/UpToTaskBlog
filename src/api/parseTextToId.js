e304f15b1e12aaaa47a5bd4cd846787b38492f45
guilherme <guilherme256teretelteltel@gmail.com>
2023-04-17 12:18:05 -0300

    change the parse text

--- src/api/parseTextToId.js
+++ src/api/parseTextToId.js
@@ -1,3 +1,3 @@
 export default (text) => {
-    return text.toLowerCase().replaceAll(' ','-').replaceAll('?','').replaceAll('!',''); 
+    return text.toLowerCase().replaceAll(' ','-').replaceAll('?','').replaceAll('!','').replaceAll('<','').replaceAll('>',''); 
 }
